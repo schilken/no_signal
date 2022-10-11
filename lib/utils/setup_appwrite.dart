02f99d09eb1acb4c15f37c155f64893f24878fae
Alfred Schilken <alfred@schilken.de>
2022-10-11 16:33:38 +0200

    replace role:all with any, define my ids

--- lib/utils/setup_appwrite.dart
+++ lib/utils/setup_appwrite.dart
@@ -6,21 +6,22 @@ import 'package:dart_appwrite/dart_appwrite.dart';
 /// Just run this function setting your endpoint, ProjectID and APIKey
 void main() async {
   Client client = Client()
-      .setEndpoint('http://appwrite.io/v1') // Replace with the endpoint
-      .setProject('[YOUR_PROJECT_ID]') // Replace with your Project ID
-      .setKey('[YOUR_SECRET_API_KEY]') // Replace with your API Key
+      .setEndpoint('https://192.168.2.23/v1') // Replace with the endpoint
+      .setProject('signal2') // Replace with your Project ID
+      .setKey(
+          '6d7b1dc4d15f9a4c78381b051c133b352305be9a8c3955653aa7275ae90dfaeda25c6d846a8e6eb61d905b5fc55f82eb8422f0a847b16ed20e47bc8124a05ac94a76477766a5f6df1673ae43e7d821cf509a7147f80e7598a7796484cc6c6f369d0a142330c25c7cc9a8d26ba38fbb08d49e127374ebdac82c6b9f2211d75db8') // Replace with your API Key
       .setSelfSigned(status: true);
 
-  Databases db = Databases(client, databaseId: 'unique()');
+  Databases db = Databases(client, databaseId: 'db');
 
-  db.create(name: '[YOUR_DATABASE_ID]');
+  await db.create(name: 'db');
 
   Storage storage = Storage(client);
   await db.createCollection(
     collectionId: 'users',
     name: 'Users',
     permission: 'document',
-    read: ["role:all"],
+    read: ["any"],
     write: [],
   );
   await db.createStringAttribute(
